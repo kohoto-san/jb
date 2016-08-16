0d757efcd57c22fb788ee099e12aa87e18636589
Michael Sinov <sihaelov@gmail.com>
2016-08-16 17:48:52 +1000

    update

--- project/project/settings.py
+++ project/project/settings.py
@@ -21,7 +21,7 @@ env = environ.Env(
     # ALLOWED_HOSTS=(list, ['127.0.0.1:8000'])
 )
 
-environ.Env.read_env()
+environ.Env.read_env('../.env')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
