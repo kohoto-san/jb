b41d7eca8e862542041949563d6c9a3336604c86
Michael Sinov <sihaelov@gmail.com>
2016-08-21 01:28:41 +1000

    update

--- project/apps/core/management/commands/parse.py
+++ project/apps/core/management/commands/parse.py
@@ -1,7 +1,11 @@
-from django.core.management.base import NoArgsCommand
+# from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand, BaseCommand
 from apps.core import parseJobs
 
 
-class Command(NoArgsCommand):
-    def handle_noargs(self, **options):
+class Command(BaseCommand):
+    help = 'Parse jobs from job boards'
+
+    # def handle_noargs(self, **options):
+    def handle(self, *args, **options):
         parseJobs.parseJobs()
