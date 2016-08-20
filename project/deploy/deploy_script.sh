c29ae6ce0d913f4e0e2d9929cade66b95e975fff
Michael Sinov <sihaelov@gmail.com>
2016-08-21 07:06:30 +1000

    update

--- project/deploy/deploy_script.sh
+++ project/deploy/deploy_script.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-python ./project/manage.py makemigrations
-python ./project/manage.py migrate
-python -c "import nltk; nltk.download('all')"
-python ./project/manage.py runserver 0.0.0.0:5000
+#python ./project/manage.py makemigrations
+#python ./project/manage.py migrate
+#python -c "import nltk; nltk.download('all')"
+#python ./project/manage.py runserver 0.0.0.0:5000
 
 # python project/manage.py collectstatic --noinput
 # gunicorn --config project/deploy/gunicorn.conf.py project.project.wsgi
