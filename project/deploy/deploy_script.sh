760e75df390912c4fc93e6093fac0c638489ab38
Michael Sinov <sihaelov@gmail.com>
2016-08-22 00:55:16 +1000

    update

--- project/deploy/deploy_script.sh
+++ project/deploy/deploy_script.sh
@@ -2,7 +2,7 @@
 
 #python ./project/manage.py makemigrations
 #python ./project/manage.py migrate
-#python -c "import nltk; nltk.download('all')"
+python -c "import nltk; nltk.download('all')"
 python ./project/manage.py runserver 0.0.0.0:5000
 
 # python project/manage.py collectstatic --noinput
