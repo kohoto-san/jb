3c66d878b9bc9646f24c473707581f8587720487
Michael Sinov <sihaelov@gmail.com>
2016-08-27 07:01:07 +1000

    update

--- project/deploy/deploy_script.sh
+++ project/deploy/deploy_script.sh
@@ -7,4 +7,6 @@
 ### python ./project/manage.py runserver 0.0.0.0:5000
 
 # python project/manage.py collectstatic --noinput
-gunicorn --config project/deploy/gunicorn.conf.py project.project.wsgi
+# gunicorn --config project/deploy/gunicorn.conf.py project.project.wsgi
+
+gunicorn project.project.wsgi --bind 0.0.0.0:5000
