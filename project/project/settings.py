dc3c93274753bf569ccbde832f0de0dd1d0464ee
Michael Sinov <sihaelov@gmail.com>
2016-08-30 03:44:19 +1000

    update

--- project/project/settings.py
+++ project/project/settings.py
@@ -208,7 +208,8 @@ STATICFILES_STORAGE = (
 
 WEBPACK_LOADER = {
     'DEFAULT': {
-        'CACHE': not DEBUG,
+        # 'CACHE': not DEBUG,
+        'CACHE': env('WEBPACK_CACHE'),
         'BUNDLE_DIR_NAME': 'bundles/', # must end with slash
         'STATS_FILE': os.path.join(BASE_DIR, '../', 'webpack-stats.json'),
         'POLL_INTERVAL': 0.1,
