4bfe152ab33f3bd1c57f50a4b0ba8cb2811bb9ef
Michael Sinov <sihaelov@gmail.com>
2016-08-31 16:49:25 +1000

    update

--- project/staticfiles/js/reducers/index.845a95da7939.js
+++
@@ -1,4 +0,0 @@
-// export {jobs, lanes} from './reducers.js'
-// export { entities, allJobs, user, data } from './reducers.js'
-// export { entities, allJobs, user, loginPopup } from './reducers.js'
-export { entities, user, loginPopup } from './reducers.js'
