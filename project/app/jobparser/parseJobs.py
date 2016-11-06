d1f11ccf5afe9979b39b190db5a03a0cf37a9b60
Michael Sinov <sihaelov@gmail.com>
2016-11-06 16:01:35 +1000

    update

--- project/app/jobparser/parseJobs.py
+++ project/app/jobparser/parseJobs.py
@@ -305,7 +305,7 @@ def _analyzeJob(name, company_name, text, skills, company_url=None, company_logo
     try:
         print(company_name)
     except UnicodeEncodeError:
-        print('unicode error — %s' % company_name.decode('ascii', 'ignore'))
+        print('unicode error')
 
     if same_company:
         print('same_company')
