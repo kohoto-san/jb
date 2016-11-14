21c866baefea1dac3899f9a11377d9912addd4a9
Michael Sinov <sihaelov@gmail.com>
2016-11-14 16:52:39 +1000

    fix screens

--- project/static/bundles/main.js
+++ project/static/bundles/main.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "6f46b79d7ff9e7dd297e"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "653716cad1839c29e534"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
